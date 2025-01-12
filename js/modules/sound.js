1ac872a33d35cf1befd7967419f21a6d460c71bf
nourabuelenin <nourabuelenin@gmail.com>
2025-01-12 23:38:43 +0200

    Properly integrated sound module with settings

--- js/modules/sound.js
+++ js/modules/sound.js
@@ -1,18 +1,13 @@
 const settingsButton = document.getElementById('settings-button');
-const settingsPopup = document.getElementById('settings-popup');
+// const settingsPopup = document.getElementById('settings-popup');
 const musicToggle = document.getElementById('music-toggle');
 const soundIcon = document.getElementById('sound-icon');
 const backgroundMusic = document.getElementById('background-music');
-const startGameButton = document.getElementById('start-restart-btn');
-const hintButton = document.getElementById('hint-button');
-const nextButton = document.getElementById('next-level');
-const saveScoreButton = document.getElementById('save-score');
-const cards = document.querySelectorAll('.card');
 const clickSound = document.getElementById('click-sound');
 let musicStarted = false;
 
 // Start background music on user interaction
-function startBackgroundMusic() {
+export function startBackgroundMusic() {
     if (!musicStarted) {
         backgroundMusic.volume = 0.5;
         backgroundMusic.play().catch(error => {
@@ -31,46 +26,8 @@ settingsButton.addEventListener('click', () => {
     playSound(clickSound);
 });
 
-settingsPopup.addEventListener('click', () => {
-    playSound(clickSound);
-});
-
-startGameButton.addEventListener('click', () => {
-    playSound(clickSound);
-});
-
-hintButton.addEventListener('click', () => {
-    playSound(clickSound);
-});
-
-nextButton.addEventListener('click', () => {
-    playSound(clickSound);
-});
-
-saveScoreButton.addEventListener('click', () => {
-    playSound(clickSound);
-});
-
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        playSound(clickSound);
-    });
-});
-
-// Toggle background music
-musicToggle.addEventListener('click', () => {
-    if (backgroundMusic.volume > 0) {
-        backgroundMusic.volume = 0;
-        soundIcon.src = 'assets/global/icons/sound-off.png';
-    } else {
-        backgroundMusic.volume = 0.5;
-        soundIcon.src = 'assets/global/icons/sound-on.png';
-    }
-    playSound(clickSound);
-});
-
 // Play a click sound
-function playSound(sound) {
+export function playSound(sound) {
     sound.currentTime = 0;
     sound.play();
 }
